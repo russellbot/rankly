8641f85a7c86e7a4efef386a6bab6cb8236c6c7d
Russell Hart <russellbot@gmail.com>
2020-09-18 16:25:07 -0400

    updated lambda function

--- handler.js
+++ handler.js
@@ -1,17 +1,21 @@
 'use strict';
 
-module.exports.rank = async event => {
-  return {
+const emojis = [
+  '😃', '😊', '😉', '😂', '🤣', '😍'
+];
+
+module.exports.rank = (event, context, callback) => {
+  const rank = event.queryStringParameters.rank;
+  const rankEmoji = emojis[rank >= emojis.length ? emojis.length - 1 : rank];
+  const response = {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: rankEmoji,
+    }),
   };
+
+  callback(null, response);
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
